6a21aa6c320202336b9d7655b9ae271912890dad
MatiasFaust <mtsfaust@gmail.com>
2026-08-10 10:08:41 -0300

    Update modu_trans.js

--- modu_trans.js
+++ modu_trans.js
@@ -1,1 +1,20 @@
+const botones = document.querySelectorAll(".opcion");
 
+botones.forEach(function(boton) {
+
+    boton.addEventListener("click", function() {
+
+        console.log("Botón presionado");
+
+    });
+
+});
+
+
+const volver = document.querySelector(".volver");
+
+volver.addEventListener("click", function() {
+
+    console.log("Volver presionado");
+
+});
