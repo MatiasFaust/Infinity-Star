ab14f714b042992f191d7f5687f0a6c4735f1263
MatiasFaust <mtsfaust@gmail.com>
2026-09-02 07:54:23 -0300

    Delete panel.js

--- panel.js
+++
@@ -1,5 +0,0 @@
-function mostrarModulo(nombre) {
-  document.getElementById("selector-modulos").style.display = nombre === "selector" ? "block" : "none";
-  document.getElementById("modulo-tras").style.display = nombre === "tras" ? "flex" : "none";
-  document.getElementById("modulo-doc").style.display = nombre === "doc" ? "flex" : "none";
-}
